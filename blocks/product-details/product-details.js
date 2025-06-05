14b23a4a737398a0e35557374069f4c992faf0cf
silviomariniCH <silviomarini@deloitte.ch>
2025-06-05 07:34:49 +0000

    day 1

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -6,85 +6,87 @@ import {
   Icon,
   Button,
   provider as UI,
-} from '@dropins/tools/components.js';
-import { events } from '@dropins/tools/event-bus.js';
-import * as pdpApi from '@dropins/storefront-pdp/api.js';
-import { render as pdpRendered } from '@dropins/storefront-pdp/render.js';
+} from "@dropins/tools/components.js";
+import { events } from "@dropins/tools/event-bus.js";
+import * as pdpApi from "@dropins/storefront-pdp/api.js";
+import { render as pdpRendered } from "@dropins/storefront-pdp/render.js";
 
 // Containers
-import ProductHeader from '@dropins/storefront-pdp/containers/ProductHeader.js';
-import ProductPrice from '@dropins/storefront-pdp/containers/ProductPrice.js';
-import ProductShortDescription from '@dropins/storefront-pdp/containers/ProductShortDescription.js';
-import ProductOptions from '@dropins/storefront-pdp/containers/ProductOptions.js';
-import ProductQuantity from '@dropins/storefront-pdp/containers/ProductQuantity.js';
-import ProductDescription from '@dropins/storefront-pdp/containers/ProductDescription.js';
-import ProductAttributes from '@dropins/storefront-pdp/containers/ProductAttributes.js';
-import ProductGallery from '@dropins/storefront-pdp/containers/ProductGallery.js';
+import ProductHeader from "@dropins/storefront-pdp/containers/ProductHeader.js";
+import ProductPrice from "@dropins/storefront-pdp/containers/ProductPrice.js";
+import ProductShortDescription from "@dropins/storefront-pdp/containers/ProductShortDescription.js";
+import ProductOptions from "@dropins/storefront-pdp/containers/ProductOptions.js";
+import ProductQuantity from "@dropins/storefront-pdp/containers/ProductQuantity.js";
+import ProductDescription from "@dropins/storefront-pdp/containers/ProductDescription.js";
+import ProductAttributes from "@dropins/storefront-pdp/containers/ProductAttributes.js";
+import ProductGallery from "@dropins/storefront-pdp/containers/ProductGallery.js";
 
 // Libs
-import { fetchPlaceholders } from '../../scripts/aem.js';
+import { fetchPlaceholders } from "../../scripts/aem.js";
 
 // Initializers
-import { IMAGES_SIZES } from '../../scripts/initializers/pdp.js';
-import '../../scripts/initializers/cart.js';
+import { IMAGES_SIZES } from "../../scripts/initializers/pdp.js";
+import "../../scripts/initializers/cart.js";
 
 export default async function decorate(block) {
   // eslint-disable-next-line no-underscore-dangle
-  const product = events._lastEvent?.['pdp/data']?.payload ?? null;
+  const product = events._lastEvent?.["pdp/data"]?.payload ?? null;
   const labels = await fetchPlaceholders();
 
   // Layout
   const fragment = document.createRange().createContextualFragment(`
-    <div class="product-details__wrapper">
-      <div class="product-details__alert"></div>
-      <div class="product-details__left-column">
-        <div class="product-details__gallery"></div>
-        <div class="product-details__maximize"></div>
-      </div>
-      <div class="product-details__right-column">
-        <div class="product-details__gallery"></div>
-        <div class="product-details__header"></div>
-        <div class="product-details__price"></div>
-        <div class="product-details__short-description"></div>
-        <div class="product-details__configuration">
-          <div class="product-details__options"></div>
-          <div class="product-details__quantity__wrapper">
-            <div class="product-details__quantity__label">
-              Quantity
+      <div class="product-details__wrapper">
+        <div class="product-details__alert"></div>
+        <div class="product-details__left-column">
+          <div class="product-details__gallery"></div>
+          <div class="product-details__maximize"></div>
+        </div>
+        <div class="product-details__right-column">
+          <div class="product-details__gallery"></div>
+          <div class="product-details__header"></div>
+          <div class="product-details__ratings"></div>
+          <div class="product-details__price"></div>
+          <div class="product-details__short-description"></div>
+          <div class="product-details__configuration">
+            <div class="product-details__options"></div>
+            <div class="product-details__quantity__wrapper">
+              <div class="product-details__quantity__label">
+                Quantity
+              </div>
+              <div class="product-details__quantity"></div>
             </div>
-            <div class="product-details__quantity"></div>
+            <div class="product-details__buttons">
+              <div class="product-details__buttons__add-to-cart"></div>
+              <div class="product-details__buttons__notify-me"></div>
+            </div>
           </div>
-          <div class="product-details__buttons">
-            <div class="product-details__buttons__add-to-cart"></div>
-            <div class="product-details__buttons__notify-me"></div>
+          <div class="product-details__description"></div>
+          <div class="product-details__attributes"></div>
+          <div class="product-details__sku">
+            Product Code: ${product.sku}
           </div>
-        </div>
-        <div class="product-details__description"></div>
-        <div class="product-details__attributes"></div>
-        <div class="product-details__sku">
-          Product Code: ${product.sku}
         </div>
       </div>
-    </div>
-  `);
+    `);
 
-  const $alert = fragment.querySelector('.product-details__alert');
-  const $gallery = fragment.querySelector('.product-details__gallery');
-  const $header = fragment.querySelector('.product-details__header');
-  const $price = fragment.querySelector('.product-details__price');
+  const $alert = fragment.querySelector(".product-details__alert");
+  const $gallery = fragment.querySelector(".product-details__gallery");
+  const $header = fragment.querySelector(".product-details__header");
+  const $price = fragment.querySelector(".product-details__price");
   const $galleryMobile = fragment.querySelector(
-    '.product-details__right-column .product-details__gallery',
+    ".product-details__right-column .product-details__gallery"
   );
   const $shortDescription = fragment.querySelector(
-    '.product-details__short-description',
+    ".product-details__short-description"
   );
-  const $options = fragment.querySelector('.product-details__options');
-  const $quantity = fragment.querySelector('.product-details__quantity');
+  const $options = fragment.querySelector(".product-details__options");
+  const $quantity = fragment.querySelector(".product-details__quantity");
   const $addToCart = fragment.querySelector(
-    '.product-details__buttons__add-to-cart',
+    ".product-details__buttons__add-to-cart"
   );
-  const $description = fragment.querySelector('.product-details__description');
-  const $attributes = fragment.querySelector('.product-details__attributes');
+  const $description = fragment.querySelector(".product-details__description");
+  const $attributes = fragment.querySelector(".product-details__attributes");
+  const $ratings = fragment.querySelector(".product-details__ratings");
 
   block.appendChild(fragment);
 
@@ -106,10 +108,10 @@ export default async function decorate(block) {
   ] = await Promise.all([
     // Gallery (Mobile)
     pdpRendered.render(ProductGallery, {
-      controls: 'thumbnailsRow',
+      controls: "thumbnailsRow",
       arrows: false,
       peak: false,
-      gap: 'small',
+      gap: "small",
       loop: false,
       imageParams: {
         ...IMAGES_SIZES,
@@ -122,10 +124,10 @@ export default async function decorate(block) {
 
     // Gallery (Desktop)
     pdpRendered.render(ProductGallery, {
-      controls: 'thumbnailsColumn',
+      controls: "thumbnailsColumn",
       arrows: false,
       peak: false,
-      gap: 'small',
+      gap: "small",
       loop: false,
       imageParams: {
         ...IMAGES_SIZES,
@@ -152,7 +154,7 @@ export default async function decorate(block) {
     // Configuration – Button - Add to Cart
     UI.render(Button, {
       children: labels.PDP?.Product?.AddToCart?.label,
-      size: 'large',
+      size: "large",
       onClick: async () => {
         try {
           addToCart.setProps((prev) => ({
@@ -168,7 +170,7 @@ export default async function decorate(block) {
           // add the product to the cart
           if (valid) {
             const { addProductsToCart } = await import(
-              '@dropins/storefront-cart/api.js'
+              "@dropins/storefront-cart/api.js"
             );
             await addProductsToCart([{ ...values }]);
           }
@@ -178,11 +180,11 @@ export default async function decorate(block) {
         } catch (error) {
           // add alert message
           inlineAlert = await UI.render(InLineAlert, {
-            heading: 'Error',
+            heading: "Error",
             description: error.message,
-            icon: Icon({ source: 'Warning' }),
-            'aria-live': 'assertive',
-            role: 'alert',
+            icon: Icon({ source: "Warning" }),
+            "aria-live": "assertive",
+            role: "alert",
             onDismiss: () => {
               inlineAlert.remove();
             },
@@ -190,8 +192,8 @@ export default async function decorate(block) {
 
           // Scroll the alertWrapper into view
           $alert.scrollIntoView({
-            behavior: 'smooth',
-            block: 'center',
+            behavior: "smooth",
+            block: "center",
           });
         } finally {
           addToCart.setProps((prev) => ({
@@ -212,24 +214,28 @@ export default async function decorate(block) {
 
   // Lifecycle Events
   events.on(
-    'pdp/valid',
+    "pdp/valid",
     (valid) => {
       // update add to cart button disabled state based on product selection validity
       addToCart.setProps((prev) => ({ ...prev, disabled: !valid }));
     },
-    { eager: true },
+    { eager: true }
   );
 
   // Set JSON-LD and Meta Tags
   events.on(
-    'eds/lcp',
+    "eds/lcp",
     () => {
       if (product) {
         setMetaTags(product);
         document.title = product.name;
+        // Ratings from gql extensibility
+        $ratings.append(
+          `${product.rating.average} stars average (out of ${product.rating.total} ratings)`
+        );
       }
     },
-    { eager: true },
+    { eager: true }
   );
 
   // eslint-disable-next-line consistent-return
@@ -247,15 +253,15 @@ function createMetaTag(property, content, type) {
       return;
     }
     meta.setAttribute(type, property);
-    meta.setAttribute('content', content);
+    meta.setAttribute("content", content);
     return;
   }
   if (!content) {
     return;
   }
-  meta = document.createElement('meta');
+  meta = document.createElement("meta");
   meta.setAttribute(type, property);
-  meta.setAttribute('content', content);
+  meta.setAttribute("content", content);
   document.head.appendChild(meta);
 }
 
@@ -264,20 +270,23 @@ function setMetaTags(product) {
     return;
   }
 
-  const price = product.prices.final.minimumAmount ?? product.prices.final.amount;
+  const price =
+    product.prices.final.minimumAmount ?? product.prices.final.amount;
 
-  createMetaTag('title', product.metaTitle || product.name, 'name');
-  createMetaTag('description', product.metaDescription, 'name');
-  createMetaTag('keywords', product.metaKeyword, 'name');
+  createMetaTag("title", product.metaTitle || product.name, "name");
+  createMetaTag("description", product.metaDescription, "name");
+  createMetaTag("keywords", product.metaKeyword, "name");
 
-  createMetaTag('og:type', 'product', 'property');
-  createMetaTag('og:description', product.shortDescription, 'property');
-  createMetaTag('og:title', product.metaTitle || product.name, 'property');
-  createMetaTag('og:url', window.location.href, 'property');
-  const mainImage = product?.images?.filter((image) => image.roles.includes('thumbnail'))[0];
+  createMetaTag("og:type", "product", "property");
+  createMetaTag("og:description", product.shortDescription, "property");
+  createMetaTag("og:title", product.metaTitle || product.name, "property");
+  createMetaTag("og:url", window.location.href, "property");
+  const mainImage = product?.images?.filter((image) =>
+    image.roles.includes("thumbnail")
+  )[0];
   const metaImage = mainImage?.url || product?.images[0]?.url;
-  createMetaTag('og:image', metaImage, 'property');
-  createMetaTag('og:image:secure_url', metaImage, 'property');
-  createMetaTag('product:price:amount', price.value, 'property');
-  createMetaTag('product:price:currency', price.currency, 'property');
+  createMetaTag("og:image", metaImage, "property");
+  createMetaTag("og:image:secure_url", metaImage, "property");
+  createMetaTag("product:price:amount", price.value, "property");
+  createMetaTag("product:price:currency", price.currency, "property");
 }
